936f2576f6878828d77eda3d340b1e0fd90b26bc
Tranq <matt.tranquada@gmail.com>
2018-04-03 17:24:23 -0400

    Add group() function to return dfs for specific groups of characters
    Added the Scanner.group() function along with global variables for
    pulling out different sets of characters from the dataframe for
    plotting.

--- scan.py
+++ scan.py
@@ -4,6 +4,22 @@ import numpy as np
 
 
 # GLOBAL VARIABLES
+CONTROLS = [x for x in range(0, 32)]
+NUMS = [x for x in range(48, 58)]
+UPPERS = [x for x in range(65, 91)]
+LOWERS = [x for x in range(97, 123)]
+PUNCS_1 = [x for x in range(32, 48)]
+PUNCS_2 = [x for x in range(58, 65)]
+PUNCS_3 = [x for x in range(91, 97)]
+PUNCS_4 = [x for x in range(123, 128)]
+EXOTICS = [x for x in range(128, 256)]
+BASICS = [9, 10, 32, 46]
+BRACKETS = [40, 41, 60, 62, 91, 93, 123, 125]
+SPLITTERS = [47, 58, 92, 124]
+HYPHENS = [45, 95]
+COMMON_PUNC = [33, 34, 39, 44, 59, 63]
+MISC_PUNC = [35, 36, 37, 38, 42, 43, 61, 64, 94, 96, 126]
+OPERATORS = [37, 42, 43, 61, 45, 95]
 
 
 # CLASS: Scanner
@@ -29,3 +45,7 @@ class Scanner(object):
         self.chars = pd.DataFrame()
         for x in self.data['counts']:
             self.chars[x] = [self.data['counts'][x]]
+        self.chars = self.chars.reindex(sorted(self.chars.columns), axis=1)
+
+    def group(self, types):
+        return self.chars[[x for x in types if x in self.chars.columns]]
