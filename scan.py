d29b73263e8b34d0cb99da64e0b48baf1e245593
Tranq <matt.tranquada@gmail.com>
2018-04-03 16:35:58 -0400

    Fix error using Pandas to store Counter output

--- scan.py
+++ scan.py
@@ -1,5 +1,4 @@
 from collections import Counter
-import pandas as pd
 import numpy as np
 
 
@@ -11,14 +10,17 @@ class Scanner(object):
     """Object for scanning text files to examine character frequencies."""
 
     def __init__(self, file, encoding="latin-1"):
+        self.encoding = encoding
         self.data = {}
-        with open(file, encoding=encoding) as f:
-            self.data['raw'] = np.array(f.readlines())
+        try:
+            with open(file, encoding=encoding) as f:
+                self.data['raw'] = np.array(f.readlines())
+        except:
+            print('Python could not interpret file using {}.'.format(encoding))
         self.data['bytes'] = np.array(
             [bytearray(x, encoding="latin-1") for x in self.data['raw']]
         )
         self.data['counts'] = Counter()
-        for x in self.data:
+        for x in self.data['bytes']:
             for c in x:
                 self.data['counts'][c] += 1
-        self.data['df'] = pd.DataFrame(self.data['counts'])
